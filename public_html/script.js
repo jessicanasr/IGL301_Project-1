b6d6056f9ce0b73b93db25a0e531d82ed862208c
jessicanasr <j_nasr@live.com>
2016-01-09 01:06:20 +0200

    js 3

---
+++ public_html/script.js
@@ -0,0 +1,10 @@
+function getName(){
+    //window.alert("1") ta2efta7 lmsg
+    var name = 
+            window.prompt("Enter your name", "LCU student");
+    document.getElementById("welcome").innerHTML = 
+            "Welcome " + name;
+}
+
+
+
